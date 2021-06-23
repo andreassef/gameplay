ed1cf7f4f6dca7af3a2828535d90514f4b169c8a
andreassef <andreassef02@gmail.com>
2021-06-23 13:27:59 -0400

    [UPDATE] - tela de login

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+    View,
+    Text,
+    Image,
+    StatusBar 
+} from 'react-native';
+import { ButtonIcon } from '../../components/ButtonIcon';
 import { styles } from './styles';
+import IllustrationImg from '../../assets/illustration.png';
 
 export function Signin() {
     return(
         <View style={styles.container}>
-            <Text>Hello World</Text>
+            <StatusBar 
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Image 
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {'\n'}
+                    suas jogatinas {'\n'}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon 
+                    title="Entrar com discord" 
+                    activeOpacity={0.6}
+                />
+            </View>
         </View>
     );
 }

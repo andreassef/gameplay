1c5c4aa335d78c09e52f4516c78972b7380454da
andreassef <andreassef02@gmail.com>
2021-06-25 00:15:03 -0400

    [UPDATE] - Refatoracoes gerais

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { 
     View,
     Text,
-    Image,
-    StatusBar 
+    Image
 } from 'react-native';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { styles } from './styles';
@@ -12,11 +11,6 @@ import IllustrationImg from '../../assets/illustration.png';
 export function Signin() {
     return(
         <View style={styles.container}>
-            <StatusBar 
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
             <Image 
                 source={IllustrationImg}
                 style={styles.image}
@@ -25,9 +19,9 @@ export function Signin() {
 
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {'\n'}
-                    suas jogatinas {'\n'}
-                    facilmente
+                    Conecte-se {'\n'}
+                    e organize suas {'\n'}
+                    jogatinas
                 </Text>
 
                 <Text style={styles.subtitle}>

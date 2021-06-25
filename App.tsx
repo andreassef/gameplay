93c715e88c08432403e671075e927d6abf462ede
andreassef <andreassef02@gmail.com>
2021-06-25 00:15:37 -0400

    [UPDATE] - Adicao de fontes, status bar e background

--- App.tsx
+++ App.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import {  Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+
 import { Signin } from './src/screens/Signin';
+import { Background } from './src/components/Background';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Signin></Signin>
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Signin></Signin>
+    </Background>
   );
 }

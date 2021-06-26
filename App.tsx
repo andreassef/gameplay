f751dcc13189dc071d60f4b1c9ce9bc7fcf7be81
andreassef <andreassef02@gmail.com>
2021-06-26 00:35:49 -0400

    [UPDATE] - Ajuste no componente principal

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { useFonts } from 'expo-font';
 import {  Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
-import { Signin } from './src/screens/Signin';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -27,7 +27,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Signin></Signin>
+      <Routes />
     </Background>
   );
 }

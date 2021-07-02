26719ba845295478843b9e70153ebab86a0e2abd
andreassef <andreassef02@gmail.com>
2021-07-02 00:15:03 -0400

    [NEW] - Componente SmallInput

---
+++ src/components/SmallInput/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { styles } from './styles';
+import { TextInput, TextInputProps } from 'react-native';
+
+export function SmallInput({...rest}: TextInputProps) {
+    return(
+        <TextInput 
+            style={styles.container}
+            keyboardType="numeric"
+            {...rest}
+        />
+    );
+}

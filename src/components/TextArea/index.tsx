29a4c71e1007db7f3a9552f57b4c9014be61bd6c
andreassef <andreassef02@gmail.com>
2021-07-02 00:15:47 -0400

    [NEW] - Componente TextArea

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { styles } from './styles';
+import { TextInput, TextInputProps } from 'react-native';
+
+export function TextArea({...rest}: TextInputProps) {
+    return(
+        <TextInput 
+            style={styles.container}
+            {...rest}
+        />
+    );
+}

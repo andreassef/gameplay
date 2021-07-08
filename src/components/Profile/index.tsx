c61cf18852630f4936607976ae35fd2ba4bfbd08
andreassef <andreassef02@gmail.com>
2021-07-08 11:03:18 -0400

    [UPDATE] - Consumindo direto da API

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './styles';
 import { Avatar } from '../Avatar';
+import { useAuth } from '../../hooks/auth';
 
 export function Profile() {
+    const { user } = useAuth();
+
     return(
         <View style={styles.container}>
-            <Avatar urlImage="https://github.com/andreassef.png" />
+            <Avatar urlImage={user.avatar} />
             <View>
                 <View style={styles.user}>
                     <Text style={styles.greeting}>
                         Olá,
                     </Text>
                     <Text style={styles.username}>
-                        André
+                        { user.firstName }
                     </Text>
                 </View>
                 <Text style={styles.message}>
-                    Hoje é dia de vitória
+                    Hello World
                 </Text>
             </View>
         </View>

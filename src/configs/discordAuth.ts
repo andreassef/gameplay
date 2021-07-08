eeb049fa9f1253e02c7f3177fc3b8ceb45ddda54
andreassef <andreassef02@gmail.com>
2021-07-07 23:36:02 -0400

    [NEW] - Configuracoes do discord

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-934d36cb-d541-453d-acdf-2af9c57dcaf5';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '862424553370943488';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENT_ID,
+    CDN_IMAGE
+}

04ae32ffa46562b505a135c2dc5b227f9bc70a2c
andreassef <andreassef02@gmail.com>
2021-07-12 21:39:17 -0400

    [ADD] - Nova funcao para pegar servidores disponiveis

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { View, FlatList } from 'react-native';
 import { Guild, GuildProps } from '../../components/Guild';
+import { Load } from '../../components/Load';
 import { ListDivider } from '../../components/ListDivider';
 
 import { styles } from './styles';
+import { api } from '../../services/api';
 
 type Props = {
     handleGuildSelected: (guild: GuildProps) => void;
 }
 
 export function Guilds({ handleGuildSelected }: Props) {
-    const guilds = [
-        {
-            id: '1',
-            name: 'Lendários',
-            icon: 'image.png',
-            owner: true
-        }
-    ];
+    const [ guilds, setGuilds ] = useState<GuildProps[]>([]);
+    const [ loading, setLoading ] = useState(true);
+
+    async function fetchGuilds() {
+        const response = await api.get('users/@me/guilds');
+        setGuilds(response.data);
+        setLoading(false);
+    }
+
+
+    useEffect(() => {
+        fetchGuilds();
+    }, []);
 
     return(
         <View style={styles.container}>
-            <FlatList 
+            {
+                loading ? 
+                <Load />
+                :
+                <FlatList 
                 data={guilds}
                 keyExtractor={item => item.id}
                 renderItem={({ item }) => (
@@ -36,6 +48,7 @@ export function Guilds({ handleGuildSelected }: Props) {
                 ListHeaderComponent={() => <ListDivider isCentered />}
                 style={styles.guilds}
             />
+            }
         </View>
     );
 }

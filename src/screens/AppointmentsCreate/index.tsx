d024932adda0d6712e55b22d8bac8497dc54b4d2
andreassef <andreassef02@gmail.com>
2021-07-12 21:45:20 -0400

    [UPDATE] - Logica de exibicao de imagem

--- src/screens/AppointmentsCreate/index.tsx
+++ src/screens/AppointmentsCreate/index.tsx
@@ -68,7 +68,7 @@ export function AppointmentsCreate() {
                 <View style={styles.form}>
                     <RectButton onPress={handleOpenGuilds}>
                         <View style={styles.select}>
-                            { guild.icon ? <GuildIcon /> : <View style={styles.image} />}
+                            { guild.icon ? <GuildIcon guildId={guild.id} iconId={guild.icon} /> : <View style={styles.image} />}
                             <View style={styles.selectBody}>
                                 <Text style={styles.label}>
                                     { guild.name ? guild.name : 'Selecione um servidor'}
